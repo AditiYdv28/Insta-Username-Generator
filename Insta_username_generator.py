fddfc2c1785baf26dfb982842863cbdae8a4562d
AditiYdv28 <94187051+AditiYdv28@users.noreply.github.com>
2022-08-19 10:13:34 +0530

    Add files via upload

---
+++ Insta_username_generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Insta username generator.")
+name = input("What is your name?\n")
+dobm = input("Enter your mother's birthday date :\n")
+dobf = input("Enter your father's birthday month :\n")
+print("This can be a good username for you :")
+user_name = name+"_"+dobm+dobf
+print(user_name)
+
